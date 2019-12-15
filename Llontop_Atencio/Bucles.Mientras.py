32214729b2082bed027e8cf9e7acbfa8c0af7893
Llontop-Atencio08 <57873068+llontop-atencio08@users.noreply.github.com>
2019-12-15 16:55:33 -0500

    se agrego el ejercicio 2 de bucles mientras

--- Llontop_Atencio/Bucles.Mientras.py
+++ Llontop_Atencio/Bucles.Mientras.py
@@ -5,3 +5,14 @@ while(edad_invalida):
     edad_invalida=(edad<25 or edad >100)
 #fin_while
 print("edad valida:",edad)
+
+
+
+#Ejercicio02
+hemoglobina=10
+hemoglobina_invalida=(hemoglobina<13 or hemoglobina>16)
+while(hemoglobina_invalida):
+    hemoglobina=float(input("ingrese hemoglobina:"))
+    hemoglobina_invalida=(hemoglobina<13 or hemoglobina>16)
+#fin_while
+print("hemoglobina valida:",hemoglobina)
