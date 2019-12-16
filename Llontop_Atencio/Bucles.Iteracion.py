fb3be6eb874347ef18739257ab4aebb69590a5bc
Llontop-Atencio08 <57873068+llontop-atencio08@users.noreply.github.com>
2019-12-15 23:54:55 -0500

    se agrego el ejercicio 2 de bucles Iteracion

--- Llontop_Atencio/Bucles.Iteracion.py
+++ Llontop_Atencio/Bucles.Iteracion.py
@@ -9,3 +9,18 @@ for letra in msg:
     if(letra=="C"):
         print("bien")
 #fin_for
+
+
+#Ejercicio02
+import  os
+msg=os.sys.argv[1]
+for letra in msg:
+    if(letra=="U"):
+        print("mamá")
+    if(letra=="E"):
+        print("abreme la puerta")
+    if(letra=="I"):
+        print("necesito dinero")
+    if(letra=="O"):
+        print("te amo")
+#fin_for
--- Llontop_Atencio/lote.bat
+++ Llontop_Atencio/lote.bat
@@ -1,3 +1,3 @@
 Python Llontop_Atencio/Bucles.Iteracion.py "felicitaciones" "muy bien" "bien"
 
-
+Python Llontop_Atencio/Bucles.Iteracion.py "mamá" "abreme la puerta" "necesito dinero" "te amo"
