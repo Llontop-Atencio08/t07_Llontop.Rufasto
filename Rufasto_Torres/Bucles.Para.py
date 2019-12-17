80786d89c1f166aeb7f3717a84f01f2e8d3d28d2
Llontop-Atencio08 <57873068+llontop-atencio08@users.noreply.github.com>
2019-12-17 08:52:04 -0500

    se agrego los 5 ejercicios de bucles Para

---
+++ Rufasto_Torres/Bucles.Para.py
@@ -0,0 +1,48 @@
+#Ejercicio01
+#Mostrar los numeros pares que hay desde el 50 al 100
+n=50
+while(n<102):
+    print(n)
+    n+=2
+#fin_while
+
+
+#Ejercicio02
+#Mostrar la suma de los 50 primeros numeros
+n=0
+s=0
+while(n<=50):
+    n=n+1
+    s=s+n
+#fin_while
+print("Suma:",s)
+
+
+#Ejercicio03
+i=0
+max=80
+while(i<=max):
+    i+=1
+    if((i%2)==0):
+        print(i)
+    #fin_si
+#fin_while
+print("Termina programa")
+
+
+#Ejercicio04
+#Mostrar los multiplos de 3 que hay desde el 3 hasta el 30
+n=3
+while(n<=30):
+    print(n)
+    n+=3
+#fin_while
+
+
+#Ejercicio05
+#Mostrar la serie
+n=0
+while(n<=60):
+    print(n)
+    n+=1
+#fin_while
