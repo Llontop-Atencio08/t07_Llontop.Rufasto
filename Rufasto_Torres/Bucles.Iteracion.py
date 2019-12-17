be2da0c2d06d82e72cf6ded7b2c28e73f9a7c906
Llontop-Atencio08 <57873068+llontop-atencio08@users.noreply.github.com>
2019-12-17 08:55:14 -0500

    se agrego los 5 ejercicios de bucles Iteracion

---
+++ Rufasto_Torres/Bucles.Iteracion.py
@@ -0,0 +1,73 @@
+#Ejercicio01
+import os
+msg=os.sys.argv[1]
+for letra in msg:
+    if(letra=="&"):
+        print("llamame ahorita")
+    if(letra=="#"):
+        print("llamame en 5 min")
+    if(letra=="@"):
+        print("ya no me llames")
+    if(letra=="/"):
+        print("donde estas")
+#fin_for
+
+
+#Ejercicio02
+import os
+msg=os.sys.argv[1]
+for letra in msg:
+    if(letra=="W"):
+        print("te amo")
+    if(letra=="X"):
+        print("te quiero")
+    if(letra=="Y"):
+        print("te adoro")
+    if(letra=="Z"):
+        print("te estimo")
+#fin_for
+
+
+#Ejercicio03
+import os
+msg=os.sys.argv[1]
+for letra in msg:
+    if(letra=="A"):
+        print("lo siento")
+    if(letra=="B"):
+        print("disculpa")
+    if(letra=="C"):
+        print("perdon")
+    if(letra=="D"):
+        print("yo no fui")
+#fin_for
+
+
+#Ejercicio04
+import os
+msg=os.sys.argv[1]
+for letra in msg:
+    if(letra=="U"):
+        print("aprobaste")
+    if(letra=="E"):
+        print("sigue asi")
+    if(letra=="I"):
+        print("esfuerzate")
+    if(letra=="O"):
+        print("estudia mas")
+#fin_for
+
+
+#Ejercicio05
+import os
+msg=os.sys.argv[1]
+for letra in msg:
+    if(letra=="1"):
+        print("ganaste una canasta navidena")
+    if(letra=="2"):
+        print("ganaste un vale de 100 soles para sodimac")
+    if(letra=="3"):
+        print("ganaste una licuadora")
+    if(letra=="4"):
+        print("ganaste un televisor de 55 pulgadas")
+#fin_for
